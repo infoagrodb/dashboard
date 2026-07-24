9e6b37714414de97a7bf53690ae8104bb3cddaf0
infoagrodb <analistacomasada@gmail.com>
2026-07-24 11:53:01 -0600

    Change CACHE_NAME to 'recuentos-campo-v11'
    Updated cache name to trigger full cache refresh.

--- apps/sw.js
+++ apps/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'recuentos-campo-v9'; // subir este número obliga a refrescar el caché entero
+const CACHE_NAME = 'recuentos-campo-v11'; // subir este número obliga a refrescar el caché entero
 const APP_SHELL = [
   './recuentosEf.html',
   './manifest.json'
